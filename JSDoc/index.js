6b9f17465ac6a2a7c6e7ffe4a8912cdc053ff695
KO <>
2023-07-30 16:07:00 +0800

    @typedef 基礎使用

--- JSDoc/index.js
+++ JSDoc/index.js
@@ -1,14 +1,29 @@
+/**
+ * @typedef {string} name
+ * @typedef {number} level
+ * @typedef {string} point
+ * 
+ * @typedef {{
+ * name: string,
+ * level: number,
+ * point?: string,
+ * }} insertData
+ * 
+ */
+
+
 
 /**
  * 創建使用者帳號
  * @param {string} create_uid 
  * @param {{
- * name: string,
- * level: number,
- * point?: string,
+ * name: name,
+ * level: level,
+ * point?: point,
  * }} insertData 
+ * @param {insertData[]} info
  */
-function insert(create_uid, insertData) {
+function insert(create_uid, insertData, info) {
     let {
         name,
         level,
--- JSDoc/test.js
+++ JSDoc/test.js
@@ -5,5 +5,11 @@ const JSDoc = require('./index');
 JSDoc.insert("ko1231", {
     name: "234",
     level: 1234,
-    // point: "1234",
-})
+    point: "1234",
+},[
+    {
+        name: "234",
+        level: 1234,
+        point: "1234",
+    }
+])
